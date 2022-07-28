612fbf2b5890238aedb92ac75714f7f1378d0cdf
jyotiraditya-mishra <110167453+jyotiraditya-mishra@users.noreply.github.com>
2022-07-28 15:53:18 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,8 @@
+                              //typed animation//
+var typed=new Typed(".typing",{
+    strings:["","web Desiner","Web Devloper","Graphic Desiner","Video editor"],
+    typeSpeed:100,
+    BackSpeed:60,
+
+    loop:true
+})
